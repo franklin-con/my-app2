f368548c566448e9385ea6d15ccff0df30e9ad4a
franklin-con <condori.pinto.frank.benjamin@gmail.com>
2020-08-21 09:58:39 -0500

    Updates

---
+++ precache-manifest.ea3264f0252f9de8f00fa0017b1a8468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1248b4822865e49b10c00327403502dc",
+    "url": "/my-app2/index.html"
+  },
+  {
+    "revision": "c78d7d2cd2a0a3667f3f",
+    "url": "/my-app2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c000be9a922bbbae844",
+    "url": "/my-app2/static/js/2.15527987.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2/static/js/2.15527987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78d7d2cd2a0a3667f3f",
+    "url": "/my-app2/static/js/main.66b5aaa5.chunk.js"
+  },
+  {
+    "revision": "b8ac5fe1496453d7da31",
+    "url": "/my-app2/static/js/runtime-main.234abd64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app2/static/media/logo.5d5d9eef.svg"
+  }
+]);
